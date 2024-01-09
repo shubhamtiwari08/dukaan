5a82c8d1b15f293dc73b601ab68bcf220ac6da12
shubham tiwari <st1173076@gmail.com>
2024-01-09 21:32:11 +0530

    fix: table alignment

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Table = ({ payout, data }) => {
   return (
     <div className=" w-full">
-      <table className="w-full text-[14px]   font-galano leading-[20px]  text-['#1a181e]">
+      <table className="w-full text-[14px] font-galano leading-[20px]  text-['#1a181e]">
         <thead className="bg-[#f2f2f2] text-[14px] font-galano leading-[20px] font text-['#1a181e] text-left  ">
           <tr>
-            <th className="py-[10px] px-[12px]  text-[#4d4d4d] rounded-l-[4px]  w-80">
+            <th className="py-[10px] px-[12px]  text-[#4d4d4d] rounded-l-[4px] w-96">
              Order ID
             </th>
-            <th className="py-[10px] px-[12px]  text-[#4d4d4d] w-80 ">
+            <th className="py-[10px] px-[12px]  text-[#4d4d4d] pl-10 w-80 ">
             Status
             </th>
             <th className="py-[10px] px-[12px]  marker: text-[#4d4d4d] text-left w-80 ">
@@ -42,7 +42,7 @@ const Table = ({ payout, data }) => {
                 <td className="py-[14px] px-[12px] text-custom-blue-400 font-semibold w-80">
                   {payout ? null : item.orderid}
                 </td>
-                <td className="py-[14px] px-[12px] flex items-center gap-[8px] text-[#1a181e]  w-80">
+                <td className="py-[14px] px-[12px] flex items-center gap-[8px] text-[#1a181e] pl-10 w-80">
                   <div
                     className={`h-[8px] w-[8px] rounded-full ${
                       item.status === "Processing"
